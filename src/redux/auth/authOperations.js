dec88c805bc6852d20cdcb57d89d2203b75b885f
Alex <gmsaleksei@gmail.com>
2021-06-09 16:28:12 +0300

    auth redux

---
+++ src/redux/auth/authOperations.js
@@ -0,0 +1,73 @@
+import axios from 'axios';
+import actions from './authActions';
+
+const {
+    registerRequest,
+    registerSuccess,
+    registerError,
+    logInRequest,
+    logInSuccess,
+    logInError,
+    logOutRequest,
+    logOutSuccess,
+    logOutError,
+    getCurrentUserRequest,
+    getCurrentUserSuccess,
+    getCurrentUserError,
+  } = actions;
+  
+axios.defaults.baseURL = 'Подставить урлу на которой будет бекенд';
+const token = {
+  set(token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+  unset() {
+    axios.defaults.headers.common.Authorization = '';
+  },
+};
+
+const getRegister = user => dispatch => {
+  dispatch(registerRequest());
+  axios
+    .post(`/signup`, user)
+    .then(({ data }) => {
+      dispatch(registerSuccess(data));
+      token.set(data.token);
+    })
+    .catch(error => dispatch(registerError(error)));
+};
+
+const getLogin = user => dispatch => {
+  dispatch(loginRequest());
+  axios
+    .post('/login', user)
+    .then(({ data }) => {
+      dispatch(loginSuccess(data));
+      token.set(data.token);
+    })
+    .catch(error => dispatch(loginError(error)));
+};
+const getUserData = () => (dispatch, getState) => {
+  const persistedToken = getState().auth.token;
+  if (!persistedToken) {
+    return;
+  }
+  token.set(persistedToken);
+  dispatch(getCurrentUserRequest());
+  axios
+    .get('/current')
+    .then(({ data }) => dispatch(getCurrentUserSuccess(data)))
+    .catch(error => dispatch(getCurrentUserError(error)));
+};
+const getLogout = () => dispatch => {
+  dispatch(logoutRequest());
+  axios
+    .post('/logout')
+    .then(() => {
+      token.unset();
+      dispatch(logoutSuccess());
+    })
+    .catch(error => dispatch(logoutError(error)));
+};
+
+export default { getRegister, getLogin, getUserData, getLogout };
---
+++ src/redux/auth/authReducer.js
@@ -0,0 +1,29 @@
+import { combineReducers } from 'redux';
+import { createReducer } from '@reduxjs/toolkit';
+import authActions from './authActions';
+
+const userInitialState = {};
+const user = createReducer(userInitialState, {
+  [authActions.logInSuccess]: (_, { payload }) => payload.userData,
+  [authActions.logOutSuccess]: () => userInitialState,
+  [authActions.getCurrentUserSuccess]: (_, { payload }) => payload,
+});
+
+const tokenInitialState = null;
+const token = createReducer(tokenInitialState, {
+  [authActions.logInSuccess]: (_, { payload }) => payload.accessToken,
+  [authActions.logOutSuccess]: () => tokenInitialState,
+});
+
+const isAuthenticated = createReducer(false, {
+  [authActions.logInSuccess]: () => true,
+  [authActions.logOutSuccess]: () => false,
+  [authActions.getCurrentUserSuccess]: () => true,
+  [authActions.getCurrentUserError]: () => false,
+});
+
+export default combineReducers({
+  user,
+  isAuthenticated,
+  token,
+});
