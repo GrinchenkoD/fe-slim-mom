c07652400e142345301f93d7cf2a073355a0404c
Vitaliy Kornienko <Kornienko.Vitaliy@gmail.com>
2021-06-10 17:32:02 +0300

    modal window complete

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Modal from '../Modal/Modal';
 
 const App = () => {
   return (
     <div>
-      
+      <h2>App</h2>
+      <Modal />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
+// import { store, persistor } from './redux/store';
+// import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-
-import { store, persistor } from './redux/store';
+import './index.css';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </PersistGate>
-  </Provider>,
+  //   <Provider store={store}>
+  //     <PersistGate loading={null} persistor={persistor}>
+  //       <BrowserRouter>
+  <App />,
+  //       </BrowserRouter>
+  //     </PersistGate>
+  //   </Provider>,
 
   document.getElementById('root'),
 );
