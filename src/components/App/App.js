eca5a6f8ac762137a7974e74a95d61569c012354
Dmytro Orgish <73847679+Dmytro888@users.noreply.github.com>
2021-06-11 18:31:59 +0300

    commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 
+import Calculator from '../Calculator/Calculator';
+
 const App = () => {
   return (
     <div>
       <Header />
-      <Main />
+      <Calculator />
+      {/* <Main /> */}
     </div>
   );
 };
