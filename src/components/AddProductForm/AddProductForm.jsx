dc2b42ceafd5aeb22e24d4d23acd096cbaa2ed7d
Alex <gmsaleksei@gmail.com>
2021-06-14 20:19:42 +0300

    working form

--- src/components/AddProductForm/AddProductForm.jsx
+++ src/components/AddProductForm/AddProductForm.jsx
@@ -1,14 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Select from 'react-select';
+import { useDispatch,useSelector } from 'react-redux';
 import sprite from '../../icons/symbol-defs.svg';
-import styles from './AddProductForm.module.css'
-import useDebounce from '../../hooks/useDebounce'
+import styles from './AddProductForm.module.css';
+import useDebounce from '../../hooks/useDebounce';
+import operations from '../../redux/products/productsOperations';
+import {getPickedDate} from '../../redux/products/productsSelectors'
 const AddProductForm = () => {
   const [searchQuerry, setSearchQuerry] = useState('');
+  const [weight,setWeight]=useState(0);
   console.log(searchQuerry);
   const [selectedOption, setSelectedOption] = useState(null);
   const debouncedSearchQuerry = useDebounce(searchQuerry, 500);
+  const dispatch=useDispatch()
+  const date=useSelector(getPickedDate);
   const handleChange = selectedOption => {
     setSelectedOption(selectedOption);
     console.log(`Option selected:`, selectedOption);
@@ -19,14 +25,24 @@ const AddProductForm = () => {
     { value: 'vanilla', label: 'Vanilla' },
   ]);
   console.log(options);
+
+  const postNewProduct=()=>{
+      const newProduct={
+          title:selectedOption.value,
+          weight,
+          date,
+      }
+      dispatch(operations.addUserProduct(newProduct))
+
+  }
   useEffect(() => {
     if (debouncedSearchQuerry) {
-    axios
-      .get(`products/${debouncedSearchQuerry}`)
-      .then(({ data }) =>
-        data.products.map(product => ({ value: product, name: product })),
-      )
-      .then(data => setOptions(data));
+      axios
+        .get(`products/${debouncedSearchQuerry}`)
+        .then(({ data }) =>
+          data.products.map(product => ({ value: product, label: product })),
+        )
+        .then(data => setOptions(data));
     }
   }, [debouncedSearchQuerry]);
 
@@ -62,11 +78,11 @@ const AddProductForm = () => {
           search
           placeholder="Your favorite drink"
         /> */}
-<input type="text"/>
+        <input name="weight" value={weight} onChange={({target})=>setWeight(target.value)} type="number" />
         <button className={styles.button} type="submit">
-        <svg className={styles.svg}>
-        <use href={sprite + '#icon-plus'}></use>
-      </svg>
+          <svg className={styles.svg}>
+            <use href={sprite + '#icon-plus'}></use>
+          </svg>
         </button>
       </form>
     </div>
