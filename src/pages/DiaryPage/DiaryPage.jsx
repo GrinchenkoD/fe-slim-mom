2a4711b131f87ad7d51882706e47a224d92f9bcc
Stanislav-Karasyk <99.kara.99@gmail.com>
2021-06-15 15:39:56 +0300

    add diaryPage

---
+++ src/pages/DiaryPage/DiaryPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './DiaryPage.module.css';
+import DiaryProductsList from '../../components/DiaryProductsList/DiaryProductsList';
+
+const DiaryPage = () => {
+  return (
+    <div>
+      <DiaryProductsList />
+    </div>
+  );
+};
+
+export default DiaryPage;
