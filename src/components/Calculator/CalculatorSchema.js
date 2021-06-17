ebfe65d4b160ef2e5f47845144016644667cfaf1
Dmitry Grinchenko <ra1d3r@i.ua>
2021-06-17 14:07:35 +0300

    q

--- src/components/Calculator/CalculatorSchema.js
+++ src/components/Calculator/CalculatorSchema.js
@@ -1,10 +1,22 @@
 import * as Yup from 'yup';
 
 const calculatorSchema = Yup.object().shape({
-  height: Yup.number().required('Обязательное поле'),
-  age: Yup.number().required('Обязательное поле'),
-  currentWeight: Yup.number().required('Обязательное поле'),
-  desiredWeight: Yup.number().required('Обязательное поле'),
+  height: Yup.number()
+    .required('Обязательное поле')
+    .positive('Не может быть меньше 0')
+    .integer(),
+  age: Yup.number()
+    .required('Обязательное поле')
+    .positive('Не может быть меньше 0')
+    .integer(),
+  currentWeight: Yup.number()
+    .required('Обязательное поле')
+    .positive('Не может быть меньше 0')
+    .integer(),
+  desiredWeight: Yup.number()
+    .required('Обязательное поле')
+    .positive('Не может быть меньше 0')
+    .integer(),
   bloodType: Yup.string().required('Обязательное поле'),
 });
 export default calculatorSchema;
