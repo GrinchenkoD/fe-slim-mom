877ec65ea520bd4b11b29840c8172cf010a0d725
Dmitry Grinchenko <ra1d3r@i.ua>
2021-06-18 16:39:47 +0300

    done

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import AuthThumb from '../../components/AuthThumb/AuthThumb'
 import Container from '../../components/Container/Container';
+import Header from '../../components/Header/Header';
 import LoginForm from '../../components/LoginForm/LoginForm'
 
 const LoginPage = () => {
     return (
         <AuthThumb>
+            <Header/>
             <Container>
                 <LoginForm />
             </Container>
